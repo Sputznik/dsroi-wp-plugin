6a2f1ea26f9b6e0288d0c7e50fdc65d5ca1481f0
Steve <anil.stephen66@gmail.com>
2024-12-19 18:25:47 +0530

    Update cpt-radical-actions post-navigation

--- partials/radical-actions/post-navigation.php
+++ partials/radical-actions/post-navigation.php
@@ -1,33 +1,31 @@
 <div class="radical-action-post-nav">
 	<?php
-		$next_post     = get_next_post();
-		$previous_post = get_previous_post();
-	?>
-	<?php if( !empty( $previous_post ) ) :
-    $previous_post_label     = "Previous archive item";
-    $previous_post_permalink = get_the_permalink( $previous_post->ID ); ?>
+		$next_post_label     = "Next archive item";
+		$previous_post_label = "Previous archive item";
+
+		$prev_nav = '
 		<div class="previous-post">
-      <a href="<?php echo $previous_post_permalink; ?>" title="<?php _e( $previous_post_label, 'dsroi-wp-plugin' ); ?>">
-        <i class="fa fa-arrow-left" aria-hidden="true"></i>
-      </a>
+			<span title="'.$previous_post_label.'"><i class="fa fa-arrow-left" aria-hidden="true"></i></span>
 			<div class="previous-post-inner">
-        <h5 class="post-label"><?php _e( $previous_post_label.":", 'dsroi-wp-plugin' ); ?></h5>
-				<a class="post-title" href="<?php echo $previous_post_permalink; ?>"><?php _e( get_the_title( $previous_post->ID ), 'dsroi-wp-plugin' ); ?></a>
+				<h5 class="post-label">'.$previous_post_label.':</h5>
+				<span class="post-title">%title</span>
 			</div>
-		</div>
-	<?php endif; ?>
-	<?php if( !empty( $next_post ) ) :
-    $next_post_label     = "Next archive item";
-    $next_post_permalink = get_the_permalink( $next_post->ID );
-  ?>
+		</div>';
+
+		$next_nav = '
 		<div class="next-post">
 			<div class="next-post-inner">
-				<h5 class="post-label next-post-label"><?php _e( $next_post_label.":", 'dsroi-wp-plugin' ); ?></h5>
-				<a class="post-title" href="<?php echo $next_post_permalink; ?>"><?php _e( get_the_title( $next_post->ID ), 'dsroi-wp-plugin' ); ?></a>
+				<h5 class="post-label next-post-label">'.$next_post_label.':</h5>
+				<span class="post-title">%title</span>
 			</div>
-      <a href="<?php echo $next_post_permalink; ?>" title="<?php _e( $next_post_label, 'dsroi-wp-plugin' ); ?>">
-        <i class="fa fa-arrow-right" aria-hidden="true"></i>
-			</a>
-		</div>
-	<?php endif; ?>
+			<span title="'.$next_post_label.'"><i class="fa fa-arrow-right" aria-hidden="true"></i></span>
+		</div>';
+
+		the_post_navigation(
+		  array(
+		    'next_text' => $next_nav,
+		    'prev_text' => $prev_nav,
+		  )
+		);
+	?>
 </div>
